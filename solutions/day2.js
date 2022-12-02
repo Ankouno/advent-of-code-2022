33c2e988eec99fa0b1c455f95f35937e4995583c
Thomas Coleman <kaizoman666@gmail.com>
2022-12-02 00:36:15 -0500

    Update day2.js

--- solutions/day2.js
+++ solutions/day2.js
@@ -1,40 +1,38 @@
 const { loadInput, printSolution } = require('../shared/common');
 
-const rounds = loadInput('inputs/day2.txt')
-  .split('\n')
-  .map(round => round.split(' '));
+const rounds = loadInput('inputs/day2.txt').split('\n');
 
 const firstSolution = () =>
   rounds.reduce((a, round) => {
-    switch (round[0] + round[1]) {
-      case 'AX': return a + 1 + 3;
-      case 'BX': return a + 1 + 0;
-      case 'CX': return a + 1 + 6;
+    switch (round) {
+      case 'A X': return a + 1 + 3;
+      case 'B X': return a + 1 + 0;
+      case 'C X': return a + 1 + 6;
 
-      case 'AY': return a + 2 + 6;
-      case 'BY': return a + 2 + 3;
-      case 'CY': return a + 2 + 0;
+      case 'A Y': return a + 2 + 6;
+      case 'B Y': return a + 2 + 3;
+      case 'C Y': return a + 2 + 0;
 
-      case 'AZ': return a + 3 + 0;
-      case 'BZ': return a + 3 + 6;
-      case 'CZ': return a + 3 + 3;
+      case 'A Z': return a + 3 + 0;
+      case 'B Z': return a + 3 + 6;
+      case 'C Z': return a + 3 + 3;
     }
   }, 0);
 
 const secondSolution = () => 
   rounds.reduce((a, round) => {
     switch (round[0] + round[1]) {
-      case 'AX': return a + 3 + 0;
-      case 'BX': return a + 1 + 0;
-      case 'CX': return a + 2 + 0;
+      case 'A X': return a + 3 + 0;
+      case 'B X': return a + 1 + 0;
+      case 'C X': return a + 2 + 0;
 
-      case 'AY': return a + 1 + 3;
-      case 'BY': return a + 2 + 3;
-      case 'CY': return a + 3 + 3;
+      case 'A Y': return a + 1 + 3;
+      case 'B Y': return a + 2 + 3;
+      case 'C Y': return a + 3 + 3;
 
-      case 'AZ': return a + 2 + 6;
-      case 'BZ': return a + 3 + 6;
-      case 'CZ': return a + 1 + 6;
+      case 'A Z': return a + 2 + 6;
+      case 'B Z': return a + 3 + 6;
+      case 'C Z': return a + 1 + 6;
     }
   }, 0);
 
