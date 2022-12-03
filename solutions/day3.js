7db045cc36d3485378532254f5e42746f61cd335
Thomas Coleman <kaizoman666@gmail.com>
2022-12-03 00:34:20 -0500

    Day 3

--- solutions/day3.js
+++ solutions/day3.js
@@ -1,11 +1,34 @@
 const { loadInput, printSolution } = require('../shared/common');
+const { intersection } = require('lodash');
 
-const input = loadInput('inputs/day3.txt').split('\n').map(Number);
+const sacks = loadInput('inputs/day3.txt').split('\n');
 
+const letterToVal = (x) => x.charCodeAt(0) + (x == x.toLowerCase() ? 1 - 97 : 27 - 65);
+
+// split each bag in half, find intersection of two halves, sum total value
 const firstSolution = () => {
+  return sacks.map(x => intersection(
+    x.substring(0, x.length / 2).split(''), 
+    x.substring(x.length / 2).split('')
+  )).reduce((a, x) =>
+    a + x.reduce((b, y) => 
+      b + letterToVal(y),
+    0),
+  0);
 }
 
+// find intersection of each group of 3 bags, sum total value
 const secondSolution = () => {
+  var badges = [];
+  for (var i = 0; i < sacks.length; i += 3) {
+    badges.push(intersection(
+      sacks[i].split(''),
+      sacks[i + 1].split(''),
+      sacks[i + 2].split('')
+    )[0]);
+  }
+  
+  return badges.reduce((a, b) => a + letterToVal(b), 0);
 }
 
 console.log("==[Day 3]=========")
---
+++ solutions/day4.js
@@ -0,0 +1,13 @@
+const { loadInput, printSolution } = require('../shared/common');
+
+const input = loadInput('inputs/day4.txt').split('\n').map(Number);
+
+const firstSolution = () => {
+}
+
+const secondSolution = () => {
+}
+
+console.log("==[Day 4]=========")
+printSolution(1, firstSolution);
+printSolution(2, secondSolution);
