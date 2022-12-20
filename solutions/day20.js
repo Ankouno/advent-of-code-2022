ef9a82c6a1abdf632365e44941bd74285b61c9ee
Thomas Coleman <kaizoman666@gmail.com>
2022-12-20 01:33:03 -0500

    Small rewrite

--- solutions/day20.js
+++ solutions/day20.js
@@ -6,10 +6,10 @@ const input = loadInput('inputs/day20.txt').split('\n').map(n => new Number(n));
  * Mixes a list of numbers by moving each individual number a number of places equal to its value.
  * Requires that the input be a list of *objects* rather than normal numbers, due to uniqueness requirements.
 */
-const mix = (input, numberOfTimes) => {
-  let mixed = [...input];
+const mix = (list, numberOfTimes) => {
+  let mixed = [...list];
   for (let i = 0; i < numberOfTimes; i++) {
-    input.forEach(n => {
+    list.forEach(n => {
       const index = mixed.indexOf(n);
       mixed.splice(index, 1);
       mixed.splice((index + n) % mixed.length, 0, n);
@@ -18,23 +18,19 @@ const mix = (input, numberOfTimes) => {
   return mixed;
 }
 
-/** Mix the input once. */
-const firstSolution = () => {
-  const output = mix(input, 1);
-  const zeroIndex = output.findIndex(n => n == 0);
-  return output[(zeroIndex + 1000) % output.length]
-       + output[(zeroIndex + 2000) % output.length]
-       + output[(zeroIndex + 3000) % output.length];
+/** Get the groove coordinates for the given list. */
+const getCoordinates = (list) => {
+  const zeroIndex = list.findIndex(n => n == 0);
+  return list[(zeroIndex + 1000) % list.length]
+       + list[(zeroIndex + 2000) % list.length]
+       + list[(zeroIndex + 3000) % list.length];
 }
 
+/** Mix the input once. */
+const firstSolution = () => getCoordinates(mix(input, 1));
+
 /** Mix the input 10 times, with each number first multiplied by a given key. */
-const secondSolution = () => {
-  let output = mix(input.map(n => new Number(n * 811589153)), 10);
-  const zeroIndex = output.findIndex(n => n == 0);
-  return output[(zeroIndex + 1000) % output.length]
-       + output[(zeroIndex + 2000) % output.length]
-       + output[(zeroIndex + 3000) % output.length];
-}
+const secondSolution = () => getCoordinates(mix(input.map(n => new Number(n * 811589153)), 10));
 
 console.log("==[Day 20]=========")
 printSolution(1, firstSolution, 13);
